34f47753c2c1ade6c1c6edccad67bf0e542c0696
Ciontuclaudia00 <ciontu.claudia.a6e@student.ucv.ro>
2021-03-29 17:46:55 +0300

    Initial commit

---
+++ SimpleMenuApp.java
@@ -0,0 +1,12 @@
+package tema2;
+
+public class SimpleMenuApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleMenu Meniu = new SimpleMenu();
+		
+		Meniu.Run();
+	}
+
+}
